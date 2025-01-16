0c4c016cca127c88a38086405c90179f5db61953
RayaneAll <9975@holbertonstudents.com>
2025-01-16 16:16:39 +0100

    Add new code for 7-islower.py

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    if c > ord('a') and c < ord('z'):
+        print(f"{c} is lower")
+    if c > ord('A') and c < ord('Z'):
+        print(f"{c} is upper")
+    if c > ord('0') and c < ord('9'):
+        print(f"{c} is upper")
