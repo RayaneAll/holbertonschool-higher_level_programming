d134497c0adce1b2b704b4ec529b8f11c7fd1181
RayaneAll <9975@holbertonstudents.com>
2025-01-16 14:23:17 +0100

    Add new code for 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print(number)
-    elif number < 10:
-        print(f"0{number}, ", end = "")
-    else:
-        print(f"{number}, ", end = "")
+for number in range(99):
+    print("{:02d}, ".format(number), end="")
+print(99)
