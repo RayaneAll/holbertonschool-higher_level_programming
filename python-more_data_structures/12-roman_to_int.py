3045ddcaf3470ad69be61b8568f5bc5726dc714c
RayaneAll <9975@holbertonstudents.com>
2025-01-21 23:23:07 +0100

    Add new code for 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     roman_numeral = {
         'I': 1,
         'V': 5,
@@ -12,10 +14,12 @@ def roman_to_int(roman_string):
     total = 0
     previous_value = 0
     for numeral in reversed(roman_string):
-        current_value = roman_numeral[numeral]
-    if current_value >= previous_value:
-        total += current_value
-    else:
-        total -= current_value
-    previous_value = current_value
+        current_value = roman_numeral.get(numeral, 0)
+        if current_value == 0:
+            return 0
+        if current_value >= previous_value:
+            total += current_value
+        else:
+            total -= current_value
+        previous_value = current_value
     return total
