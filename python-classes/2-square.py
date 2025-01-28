9513aa89b65c494e633d5bd2e807a71d16a6f0cb
RayaneAll <9975@holbertonstudents.com>
2025-01-28 20:01:38 +0100

    Add code for 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""Defines an empty class Square."""
 
+
+class Square:
+    """An empty class that defines a square."""
+    def __init__(self, size=0):
+        self.__size = size
+        try:
+            size is int
+        except:
+            raise TypeError("size must be an integer")
+        try:
+            size >= 0
+        except:
+            raise ValueError("size must be >= 0")
