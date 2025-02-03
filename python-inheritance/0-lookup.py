b32412a3bb1f70ccb0be789c2bbd147d8632dac5
RayaneAll <9975@holbertonstudents.com>
2025-02-03 19:37:40 +0100

    Add new code to the file 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """Lookup"""
 
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
-    return (dir(obj))
+    return dir(obj)
